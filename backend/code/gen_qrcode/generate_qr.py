5249b829a72e878a291af384d42f3d84e243c059
NzKoff <nskvip54@gmail.com>
2020-11-15 00:32:11 +0700

    add gps couriers

--- backend/code/gen_qrcode/generate_qr.py
+++ backend/code/gen_qrcode/generate_qr.py
@@ -5,7 +5,7 @@ import datetime
 def generate(data):
     img_name = "qr-{}.png".format(datetime.datetime.now()).replace(" ", "_").replace("-", "_").replace(":", "_")
     img = qrcode.make(data, border=1)
-    img.save(img_name)
+    img.save("../static/{}".format(img_name))
     return img
 
 #generate(url)
