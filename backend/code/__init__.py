b76664fa5ace4307279bf04962d654ceb0e3abe7
test@test.ru <test@test.ru>
2020-11-14 13:58:46 +0000

    Fix

---
+++ backend/code/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_restplus import Api
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+api = Api(app=app)
+
+
+import code.test.processor
