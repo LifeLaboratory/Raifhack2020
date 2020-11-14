37eae0b01cc42dad6e083aaa16e4401c2b2dfc49
Роман <djurball@list.ru>
2020-11-14 16:32:17 +0700

    init backend

---
+++ backend/test/processor.py
@@ -0,0 +1,20 @@
+from backend import api
+from flask_restplus import Resource, fields
+from backend.test.provider import Provider
+
+a_test = api.model('Test', {'msg': fields.String('PONG!')})
+a_test2 = api.model('Test2', {'data': fields.String('test_data')})
+
+
+@api.route('/api/test')
+class Test(Resource):
+
+    def get(self, **kwargs):
+        return Provider.ping()
+
+
+@api.route('/api/test2')
+class Test2(Resource):
+
+    def get(self, **kwargs):
+        return Provider.test_data(123)
---
+++ backend/test/provider.py
@@ -0,0 +1,12 @@
+from backend import api
+from flask_restplus import Resource, Api
+
+
+class Provider:
+    @classmethod
+    def ping(cls):
+        return {'msg': 'PONG!'}
+
+    @classmethod
+    def test_data(cls, data):
+        return {'test_data': data}
