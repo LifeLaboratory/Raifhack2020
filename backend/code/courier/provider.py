872bf578bb096d222653617116e36e003d2599f9
NzKoff <nskvip54@gmail.com>
2020-11-14 23:15:44 +0700

    add requests

--- backend/code/courier/provider.py
+++ backend/code/courier/provider.py
@@ -1,12 +1,12 @@
 from code import api
 from flask_restplus import Resource, Api
+from code.base.base_sql import Sql
+from code.sql.sql_query import *
 
 
 class Provider:
+    
     @classmethod
-    def ping(cls):
-        return {'msg': 'PONG!'}
-
-    @classmethod
-    def test_data(cls, data):
-        return {'test_data': data}
+    def get_courier_orders(cls, courier_id):
+        query = SQL_SELECT_ORDERS_COURIER.format(id=courier_id)
+        return Sql.exec(query=query)
